46f8d9601b7dca934e67c125a48a41fb16e91367
Ivan <94319792+Sopi197@users.noreply.github.com>
2024-12-14 22:50:48 +0300

    Update index.php

--- books/Новая папка/index.php
+++ books/Новая папка/index.php
@@ -1,14 +1,63 @@
 <?php
 
-// php в примерах и задачах [Васильев] - 99 стр. Цикл по массиву
+// php в примерах и задачах [Васильев] - 110 стр. Присваивание массивов
 
 // В качестве индексов массива можно использовать не только целые числа, но и текст. В таком случае вместо термина «индекс», как правило, используют термин «ключ». Массив с текстовыми ключами обычно называют ассоциативным массивом.
 // массив с индексными ключами == список
 // ассоциативный массив == словарь, таблица
 
-$arr = [1, 2, false];
+$arr = ["one" => 1, "two" => 2, "three" => "str"];
 
-// вордпресс курс
-// https://www.youtube.com/watch?v=eoexOZkswhQ&list=PL3LQJkGQtzc66eiaqOVgIVQ5mw4_6KqDd&index=3&ab_channel=GloAcademy
-// https://www.youtube.com/watch?v=ApZV2z46nd4&t=518s&ab_channel=%D0%9E%D1%820%D0%B4%D0%BE1
-// 
+// альтернативная запись foreach
+foreach($arr as $key => $value):
+    if(is_string($value)) {
+        echo "$value - удаляем \n";
+        unset($arr[$key]);
+    }
+    else {
+        echo "$value - оставляем \n";
+    }
+endforeach;
+print_r($arr);
+
+$x = 10;
+$y = $x;
+$y = 10 + 10;
+var_dump($x); // 10
+var_dump($y); // 20
+
+// передача переменной по ссылке &. Переменные $x и $y ссылаются на одну и ту же область памяти
+$x = 10;
+$y = &$x;
+$y = 10 + 1;
+var_dump($x); // 11
+var_dump($y); // 11
+
+$arr = [100, 200, 300];
+
+foreach ($arr as $value):
+    $value++;
+endforeach;
+print_r($arr); // [100, 200, 300] - значение массива не меняется
+
+foreach ($arr as &$value):
+    $value++;
+endforeach;
+print_r($arr); // [101, 201, 301]
+
+$complex_arr = [[1, 2, 3], ["10", 33, 100], ['str', 10, "hello"]];
+
+for ($i = 0; $i < count($complex_arr); $i++) {
+    for ($k = 0; $k < count($complex_arr[$i]); $k++) {
+        echo "Элемент массива: " . $complex_arr[$i][$k] . "\n";
+    }
+    echo "\n";
+}
+// или
+foreach ($complex_arr as $key => $value) {
+    foreach ($value as $val) {
+        echo "Элемент массива: " . $val . " \n";
+    }
+    echo "\n";
+}
+
